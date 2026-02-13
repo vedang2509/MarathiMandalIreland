9bdbecbf8f442225181856a08841308320edb3fa
vedang2509 <vedangbhide.5583@gmail.com>
2026-02-13 17:11:13 +0000

    Update TicketTailorTest.jsx

--- src/pages/TicketTailorTest.jsx
+++ src/pages/TicketTailorTest.jsx
@@ -21,7 +21,7 @@ export default function TicketTailorTest() {
       >
         <iframe
           title="Ticket Tailor Checkout"
-          src="https://www.tickettailor.com/checkout/view-event/2046923/chk/"
+          src="https://www.tickettailor.com/events/marathimandalireland1/2046923"
           width="100%"
           height="100%"
           frameBorder="0"
