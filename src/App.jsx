9c6a9d35adee625c0dad5bf96ecdb93614fc9580
vedang2509 <vedangbhide.5583@gmail.com>
2026-02-13 16:44:09 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import BlogPost from "./pages/BlogPost";
 import VarshikAnkReader from "./pages/VarshikAnkReader";
+import PaypalTest from "./pages/PaypalTest";
 
 
 import Home from './pages/Home'
